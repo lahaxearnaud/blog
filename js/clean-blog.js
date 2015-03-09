ce167a7c2ff1f7b5c4a666405bac915bb24930a4
LAHAXE Arnaud <Arnaud.LAHAXE.ext@boursorama.fr>
2015-03-09 12:21:01 +0100

    menache

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -79,6 +79,7 @@ $(function() {
       searchInput: document.getElementById('search-input'),
       resultsContainer: document.getElementById('results-container'),
       dataSource: '/search.json',
+      searchResultTemplate: '<li class="list-group-item"><a href="{url}">{title}</a></li>'
     });
 });
 
